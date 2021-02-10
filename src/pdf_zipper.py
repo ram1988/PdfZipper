981a490a3a873a7a64cdae24c12744e5083837c0
ramnarayan <ramnarayan.cse@gmail.com>
2021-02-10 21:16:03 +0100

    pdf zipper

--- src/pdf_zipper.py
+++ src/pdf_zipper.py
@@ -13,10 +13,12 @@ def hello():
 def zip_request():
   if request.method == 'POST':
     memory_file = io.BytesIO()
+    # In--memory zip file handling
     with ZipFile(memory_file,'w', compression = ZIP_DEFLATED) as zf: # Compression type
       json_str = request.form['json']
       uploaded_pdf_file = request.files['pdf_file']
       zf.writestr(uploaded_pdf_file.filename, uploaded_pdf_file.read())
       zf.writestr('content.json', json_str)
+    # After writing the contents to zip file, the file pointer is reset to the starting position.
     memory_file.seek(0)
     return send_file(memory_file, attachment_filename='compressed_response.zip', as_attachment=True)
